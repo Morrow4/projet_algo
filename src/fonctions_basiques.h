319ab2b55c3ecc9235cced97d0d33248a4bf6944
morrow <clement.laguiseray@gmail.com>
2024-01-19 11:31:27 +0100

    .h

--- src/fonctions_basiques.h
+++ src/fonctions_basiques.h
@@ -10,7 +10,7 @@
 
 node* create_node(int data);
 arc* create_arc(node* destination, int data);
-void add_arc(node* source, node* destination, int data);
+arc* add_arc(node* source, node* destination, int data);
 graph* create_graph(int nb_nodes);
 void print_graph(graph* graph);
 void delete_graph(graph* graph)
