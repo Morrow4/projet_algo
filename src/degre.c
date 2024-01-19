6445337bee231bd87e082aac4ad98c441f6dc415
morrow <clement.laguiseray@gmail.com>
2024-01-19 11:52:46 +0100

    création degre

---
+++ src/degre.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+// fonction de calcul du degré entrant d'un sommet
+// fonction de calcul du degré sortant d'un sommet
+// fonction de calcul du degré d'un sommet
