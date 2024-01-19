79cbce2f1516ead5e14e1accabe4c608c30b0b4d
morrow <clement.laguiseray@gmail.com>
2024-01-19 10:00:48 +0100

    prévision fonction à créer

--- src/fonctions_basiques.c
+++ src/fonctions_basiques.c
@@ -3,3 +3,8 @@
 
 /* Fonctions basiques pour la structure de graphe */
 
+// fonction de création de graphes
+// fonction d'ajout des sommets
+// fonction d'ajout des arcs
+// fonction pour supprimmer la structure de graphe
+// fonction pour supprimmer les noeuds et arrêtes
