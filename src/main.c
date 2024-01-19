599a24e8a1a138c2e5a6bdcec65d83134205cae7
morrow <clement.laguiseray@gmail.com>
2024-01-19 11:05:11 +0100

    print_graph

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main{
     add_arc(&my_graph -> nodes[0], &my_graph -> nodes[1], 5);
     add_arc(&my_graph -> nodes[0], &my_graph -> nodes[2], 10);
     add_arc(&my_graph -> nodes[1], &my_graph -> nodes[2], 2);
-    
+
+    //print_graph(my_graph);
     return 0;
 }
