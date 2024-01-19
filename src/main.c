7e8e4431cce17c1f96dbb52045584814650beabc
morrow <clement.laguiseray@gmail.com>
2024-01-19 11:04:12 +0100

    exemple d'utilisation

--- src/main.c
+++ src/main.c
@@ -4,6 +4,12 @@
 #include "fonctions_basiques.h"
 
 int main{
+    // Exemple d'utilisation
+    graph* my_graph = create_graph(3);
 
-    return 1;
+    add_arc(&my_graph -> nodes[0], &my_graph -> nodes[1], 5);
+    add_arc(&my_graph -> nodes[0], &my_graph -> nodes[2], 10);
+    add_arc(&my_graph -> nodes[1], &my_graph -> nodes[2], 2);
+    
+    return 0;
 }
