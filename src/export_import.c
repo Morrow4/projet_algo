c5289c3cde1e8a116e477ac9e79637540e9121f9
Morrow <clement.laguiseray@gmail.com>
2024-01-22 10:24:33 +0100

    1ère foncton ?

--- src/export_import.c
+++ src/export_import.c
@@ -3,14 +3,27 @@
 #include "struct.h"
 #include "fonctions_basiques.h"
 
-void export_graph(char* filename, char* path {
+void export_graph(char* filename, graph* graph) {
     FILE* input_file;
-    input_file = fopen(filename, "w+");
+    char* buff;
+    input_file = fopen(filename, "w");
     if (inputFile == NULL) {
         printf("Cannot open file %s\n", filename);
         exit(0);
     }
-    // code 
+    for (int i = 0; i < graph -> nb_nodes; i++) {
+        int node = graph -> nodes[i].data;
+        sprintf(buff,"%d", node);
+        fprintf(FILE* input_file, "buff\n");
+        arc* current_arc = graph -> nodes[i].arcs;
+        while (current_arc != NULL) {
+            int arc = current_arc -> data;
+            sprintf(buff,"%d", arc);
+            fprintf(FILE* input_file, "buff\n");
+            current_arc = current_arc -> next;
+        }
+        printf("\n"); // saut de ligne à la fin de chaque noeud
+    }
     fclose(input_file);
 }
 
@@ -21,6 +34,6 @@ void import_graph(char* filename) {
         printf("Cannot open file %s\n", filename);
         exit(0);
     }
-    // code 
+    fscanf()
     fclose(input_file);
 }
