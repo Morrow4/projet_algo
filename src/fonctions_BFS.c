eaec9c2e9d864591354ff8a7229a2c26ba857a24
Morrow <clement.laguiseray@gmail.com>
2024-01-22 09:36:41 +0100

    espace

--- src/fonctions_BFS.c
+++ src/fonctions_BFS.c
@@ -6,34 +6,29 @@
 
 // Fonction de parcours en largeur (BFS)
 void bfs(graph* graph, int start_node_index) {
-    if (graph == NULL || start_node_index < 0 || start_node_index >= graph->nb_nodes) {
+    if (graph == NULL || start_node_index < 0 || start_node_index >= graph -> nb_nodes) {
         return;
     }
-
     queue* q = create_queue();
-    int* visited = (int*)calloc(graph->nb_nodes, sizeof(int));
-
+    int* visited = (int*)calloc(graph -> nb_nodes, sizeof(int));
     if (q == NULL || visited == NULL) {
         return;
     }
-
-    enqueue(q, &(graph->nodes[start_node_index]));
+    enqueue(q, &(graph -> nodes[start_node_index]));
     visited[start_node_index] = 1;
-
     while (!is_queue_empty(q)) {
         node* current_node = dequeue(q);
-        printf("Visited Node: %d\n", current_node->data);
+        printf("Visited Node: %d\n", current_node -> data);
 
-        arc* current_arc = current_node->arcs;
+        arc* current_arc = current_node -> arcs;
         while (current_arc != NULL) {
-            if (!visited[current_arc->destination->data]) {
-                enqueue(q, current_arc->destination);
-                visited[current_arc->destination->data] = 1;
+            if (!visited[current_arc->destination -> data]) {
+                enqueue(q, current_arc -> destination);
+                visited[current_arc -> destination -> data] = 1;
             }
             current_arc = current_arc->next;
         }
     }
-
     free(visited);
     free(q);
 }
--- src/fonctions_DFS.c
+++ src/fonctions_DFS.c
@@ -6,34 +6,29 @@
 
 // Fonction de parcours en profondeur (DFS)
 void dfs(graph* graph, int start_node_index, int* visited) {
-    if (graph == NULL || start_node_index < 0 || start_node_index >= graph->nb_nodes) {
+    if (graph == NULL || start_node_index < 0 || start_node_index >= graph -> nb_nodes) {
         return;
     }
-
-    printf("Visited Node: %d\n", graph->nodes[start_node_index].data);
+    printf("Visited Node: %d\n", graph -> nodes[start_node_index].data);
     visited[start_node_index] = 1;
-
-    arc* current_arc = graph->nodes[start_node_index].arcs;
+    arc* current_arc = graph -> nodes[start_node_index].arcs;
     while (current_arc != NULL) {
-        if (!visited[current_arc->destination->data]) {
-            dfs(graph, current_arc->destination->data, visited);
+        if (!visited[current_arc -> destination -> data]) {
+            dfs(graph, current_arc -> destination -> data, visited);
         }
-        current_arc = current_arc->next;
+        current_arc = current_arc -> next;
     }
 }
 
 // Fonction d'initialisation pour le parcours en profondeur (DFS)
 void dfs_traversal(graph* graph, int start_node_index) {
-    if (graph == NULL || start_node_index < 0 || start_node_index >= graph->nb_nodes) {
+    if (graph == NULL || start_node_index < 0 || start_node_index >= graph -> nb_nodes) {
         return;
     }
-
-    int* visited = (int*)calloc(graph->nb_nodes, sizeof(int));
+    int* visited = (int*)calloc(graph -> nb_nodes, sizeof(int));
     if (visited == NULL) {
         return;
     }
-
     dfs(graph, start_node_index, visited);
-
     free(visited);
 }
