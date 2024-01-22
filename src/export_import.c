d6f7c0249bd4c5fb4f9bc7778834b82efc37a6f2
Morrow <clement.laguiseray@gmail.com>
2024-01-22 11:57:53 +0100

    import

--- src/export_import.c
+++ src/export_import.c
@@ -6,23 +6,42 @@
 char* export_graph(char* filename, graph* graph) {
     FILE *input_file;
     char *buff;
+    int node, ID;
     input_file = fopen(filename, "w");
+    
     int count = graph -> nb_nodes;
     sprintf(buff,"%d", count);
     fprintf(FILE* input_file, "count\n");
+
     if (inputFile == NULL) {
         printf("Cannot open file %s\n", filename);
         exit(0);
     }
     for (int i = 0; i < graph -> nb_nodes; i++) {
-        int node = graph -> nodes[i].data;
-        sprintf(buff,"%d", node);
+
+        node = graph -> nodes[i].data;
+        sprintf(buff, "%d", node);
+        fprintf(FILE* input_file, "buff");
+
+        ID = graph -> nodes[i].ID;
+        sprintf(buff,"%d", ID);
         fprintf(FILE* input_file, "buff\n");
         arc *current_arc = graph -> nodes[i].arcs;
+
         while (current_arc != NULL) {
+
+            ID = current_arc -> destination -> ID;
+            sprintf(buff, "%d", ID);
+            fprintf(FILE* input_file, "buff");
+
+            node = current_arc -> destination -> data;
+            sprintf(buff, "%d", node);
+            fprintf(FILE* input_file, "buff");
+
             int arc = current_arc -> data;
             sprintf(buff,"%d", arc);
             fprintf(FILE* input_file, "buff\n");
+
             current_arc = current_arc -> next;
         }
         printf("\n"); // saut de ligne à la fin de chaque noeud
@@ -43,7 +62,7 @@ graph* import_graph(char* filename) {
         exit(0);
     }
     while (fgets(buff, sizeof(buff), input_file) != NULL) {
-        create_node(buff);
+        add_node(buff);
         if (node_destination == NULL) {
             create_node(data_destination);
         }
