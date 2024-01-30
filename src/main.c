d4c08346984abd7d68394ffea09929be68ec228e
Morrow <clement.laguiseray@gmail.com>
2024-01-30 19:05:14 +0100

    a

--- src/main.c
+++ src/main.c
@@ -7,10 +7,6 @@ int main() {
     // Exemple d'utilisation
     struct graph* my_graph = create_graph(3);
 
-    add_arc(&my_graph -> nodes[0], &my_graph -> nodes[1], 5);
-    add_arc(&my_graph -> nodes[0], &my_graph -> nodes[2], 10);
-    add_arc(&my_graph -> nodes[1], &my_graph -> nodes[2], 2);
-
     print_graph(my_graph);
     
     //bfs(my_graph, 0); // A finir
