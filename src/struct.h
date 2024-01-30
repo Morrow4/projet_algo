079f5d1b170c17e1b678788981282e3f7876a6ae
Morrow <clement.laguiseray@gmail.com>
2024-01-30 19:04:17 +0100

    t

--- src/struct.h
+++ src/struct.h
@@ -11,28 +11,28 @@ struct node;
 // Structure de graphe
 typedef struct graph {
     int nb_nodes; // nombre total de noeuds
-    struct node *nodes; // liste qui pointe vers les noeuds du graphe
+    struct node* head; // tete du graphique
 } graph;
 
 // Structure d'arrête de graphe
 typedef struct arc {
     struct node* destination; // pointeur vers le noeud de destination
     int data; // valeur de l'arrête
-    struct arc *next; // pointeur vers la prochaine arête dans la liste
+    struct arc* next; // pointeur vers la prochaine arête dans la liste
 } arc;
 
 // Structure de noeud de graphe
 typedef struct node {
     int ID; // node ID
     int data; // valeur du noeud
-    struct arc *arc; // liste des arêtes connectés à ce noeud
+    struct arc* arc; // liste des arêtes connectés à ce noeud
 } node;
 
 // Structure de file
 typedef struct queue_element
 {
-    struct queue_element *p;
-    struct queue_element *n;
+    struct queue_element* p;
+    struct queue_element* n;
     struct node* node;
 } queue_element;
 
