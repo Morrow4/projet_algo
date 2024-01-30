685dc082cdaeb57d915dba090834eda76be257f2
Morrow <clement.laguiseray@gmail.com>
2024-01-30 18:19:04 +0100

    test

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@ int main() {
     bfs(my_graph, 0); // A finir
     depth_first_search(my-graph, 0);$
     
-    sousgraphe1(my_graph, int* tabIndice, int nb_nodes); // A finir
-    sousgraphe2(my_graph, int* tabArcs, int nb_arcs); // A finir
+    sousgraphe1(my_graph, tabIndice, nb_nodes); // A finir
+    sousgraphe2(my_graph, tabArcs, nb_arcs); // A finir
     delete_graph(my_graph); // A finir
     return 0;
 }
