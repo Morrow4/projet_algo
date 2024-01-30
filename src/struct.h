43bb7d381dff9a71f92428385cfebfefbf2a8ce9
boblipopi <jarcadias@gmail.com>
2024-01-30 18:50:38 +0100

    push

--- src/struct.h
+++ src/struct.h
@@ -12,21 +12,21 @@ struct node;
 typedef struct graph {
     int nb_nodes; // nombre total de noeuds
     struct node *nodes; // liste qui pointe vers les noeuds du graphe
-};
+} graph;
 
 // Structure d'arrête de graphe
 typedef struct arc {
     struct node* destination; // pointeur vers le noeud de destination
     int data; // valeur de l'arrête
     struct arc *next; // pointeur vers la prochaine arête dans la liste
-};
+} arc;
 
 // Structure de noeud de graphe
 typedef struct node {
     int ID; // node ID
     int data; // valeur du noeud
     struct arc *arc; // liste des arêtes connectés à ce noeud
-};
+} node;
 
 // Structure de file
 typedef struct queue_element
@@ -34,17 +34,17 @@ typedef struct queue_element
     struct queue_element *p;
     struct queue_element *n;
     struct node* node;
-};
+} queue_element;
 
 typedef struct queue 
 {
     struct queue_element* head;
-};
+} queue;
 
 // Structure de pile pour stocker les nœuds à visiter
 typedef struct stack {
     struct node* data;
     struct stack* n;
-};
+} stack;
 
 #endif STRUCT_H
