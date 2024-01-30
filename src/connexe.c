8f1454e4ce547ffcee0e4bda8d9a5b3ea587c72d
Morrow <clement.laguiseray@gmail.com>
2024-01-30 21:24:34 +0100

    t

--- src/connexe.c
+++ src/connexe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "struct.h"
+#include "fonctions.h"
 
 // Fonction auxiliaire pour vérifier la connectivité forte en utilisant DFS
 int dfs_strongly_connected(struct graph* graph, int node_id) {
