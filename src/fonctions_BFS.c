36792e79059c6271d14f2c8360d53604b8028d70
Morrow <clement.laguiseray@gmail.com>
2024-01-30 17:03:15 +0100

    changes

--- src/fonctions_BFS.c
+++ src/fonctions_BFS.c
@@ -32,3 +32,36 @@ void bfs(graph* graph, int start_node_index) {
     free(visited);
     free(q);
 }
+
+/* Breadth-First Search (BFS) */
+
+struct node* bfs_arbre(struct graph* arbre, int val) {
+    struct queue q;
+    q.tete = NULL;
+    struct node* node;
+    if (arbre -> racine == NULL) {
+        return NULL ;
+    } 
+    if (arbre -> racine -> donnee == val) {
+        printf("%d a été trouvé\n", val);
+        return arbre -> racine ;
+    }
+    queue_push(&q, arbre -> racine); // Ajoute la racine à la file
+    while (!is_empty(&q)) { // Tant que la file n'est pas vide
+        tmp_noeud = queue_pop(&q); // Retire un sommet et ajouter les autres sommets connectés
+        if (tmp_noeud -> l != NULL) {  // Test à gauche si le noeud existe
+            if (tmp_noeud -> l -> donnee == val) { // Test à gauche si la valeur contenu dans le noeud est celle recherché
+                printf("%d a été trouvé\n", val);
+                return tmp_noeud -> l, tmp_noeud ;
+            }
+            queue_push(&q, tmp_noeud -> l); // Sinon ajoute le noeud à gauche à la file
+        }
+        if (tmp_noeud -> r != NULL) { // Test à droite si le noeud existe
+            if (tmp_noeud -> r -> donnee == val) { // Test à droite si la valeur contenu dans le noeud est celle recherché
+                printf("%d a été trouvé\n", val);
+                return tmp_noeud -> r, tmp_noeud ;
+            }
+            queue_push(&q, tmp_noeud -> r); // Sinon ajoute le noeud à droite à la file
+        }
+    }
+}
--- src/struct.h
+++ src/struct.h
@@ -10,32 +10,33 @@
 typedef struct graph {
     int nb_nodes; // nombre total de noeuds
     node *nodes; // liste qui pointe vers les noeuds du graphe
-} graph;
+};
 
 // Structure d'arrête de graphe
 typedef struct arc {
     node* destination; // pointeur vers le noeud de destination
     int data; // valeur de l'arrête
     struct arc *next; // pointeur vers la prochaine arête dans la liste
-} arc;
+};
 
 // Structure de noeud de graphe
 typedef struct node {
     int ID; // node ID
     int data; // valeur du noeud
     struct arcs *arc; // liste des arêtes connectés à ce noeud
-} node;
+};
 
-// Structure element
-typedef struct Element {
-    int valeur;
-    struct Element* suivant;
-} Element;
+// Structure de file
+typedef struct queue_element
+{
+    struct queue_element *p;
+    struct queue_element *n;
+    struct node* node;
+};
 
-// Structure file
-typedef struct File {
-    Element* tete;
-    Element* queue;
-} File;
+typedef struct queue 
+{
+    struct queue_element* tete;
+};
 
 #ENDIF STRUCT_H
